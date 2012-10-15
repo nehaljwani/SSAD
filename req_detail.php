2ee3612d9cea8e8268f35afb0e39682660b4f3b4
shubham sangal <shubham@shubham-HP-dv6.(none)>
2012-10-16 04:08:22 +0530

    Request Details of a particular request

---
+++ req_detail.php
@@ -0,0 +1,53 @@
+<?php
+include("essential.php");
+dbconnect();
+$id=19;
+$sq="select * from Requests where reqNo =".$id.";";
+//echo $sq;
+$res=execute($sq);
+$col1=array("RequestNO","hash","Creator","creatorEmail","creatorPhone ","creatorPhone ","concernedPersonEmail"," concernedPPhone","appStatus"," reqGId","reqDate","eventStartDate","eventEndDate","eventStartTime","eventEndTime ","eventTitle","eventDesc","eventDays","concernedAdmin","room","reqType");
+$num_row=mysql_num_fields($res);
+$col=mysql_fetch_row($res);
+
+
+?> 
+<html>
+<title>Request Details</title>
+<body>
+<?php include("header.php"); ?>
+<h2 align='center'>Request Details</h2>
+<table id="box-table-a">
+<thead>
+<tr>
+<th scope="col">Fields</th>
+<th scope="col">Details</th>
+</tr>
+</thead>
+
+
+<?php while($i<$num_row){
+if($i!=1)
+{
+?>
+<tr>
+<td>
+<?php
+echo $col1[$i];
+?>
+</td>
+<td>
+<?php         
+echo $col[$i]; 
+?>
+</td>
+</tr>
+<?php
+}
+$i++;
+}
+?>
+</table>
+<?php include("footer.php"); ?>
+</body>
+</html>
+
