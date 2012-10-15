cbf686cedd19a0d589d2aa1e9d9673b8b3d3b831
shubham sangal <shubham@shubham-HP-dv6.(none)>
2012-10-15 22:52:53 +0530

    few forms

---
+++ s_Addbuilding.php
@@ -0,0 +1,48 @@
+<?php
+include("shubham_essential.php");
+dbconnect();
+if(isset($_GET['msg'])){
+        echo "<p style='background-color:#1C478E;font-size:14pt;color:#FFFFFF;text-align:right;'>" . $_GET['msg'] . "</p>";
+}
+
+$query = "select buildingName from Building;";
+$r='bname';
+$list1=generate_list($query,$r);
+
+?>
+
+
+<html>
+<title>Add Building</title>
+<head>
+<script type='text/javascript' src='livevalidation_standalone.js'></script>
+</head>
+<body>
+<form name='insert' action='s_addbuild2.php' method='post'>
+<h2 align='center'>Add Building</h2>
+<table align='center'>
+
+<tr><td>
+Enter the new Building's Name : </td><td><input type='text' id='enter' name = 'Bname' />
+<script type="text/javascript">
+var f2 = new LiveValidation('enter');
+f2.add( Validate.Presence );
+</script>
+
+</td>
+</tr>
+<tr><td></td><td><input type='submit' name='add' value='Add'/></td>
+</tr></table>
+</form><br/><br/><br/>
+<h2 align='center'> Delete Building </h2>
+<form name='delete' action='s_addbuild2.php' method='post'>
+<table align='center'>
+<tr><td>
+Building Name: </td>
+<td><?php echo $list1 ?></td>
+</tr><tr><td></td><td><input type='submit' name='delete' value='Delete' /></td>
+</tr></table>
+</form>
+</body>
+</html>
+
---
+++ s_addbuild2.php
@@ -0,0 +1,40 @@
+<?php
+include("shubham_essential.php");
+dbconnect();
+
+if(isset($_POST['delete']))                                     // if to delete a building
+{
+        if(empty($_POST['bname']))
+        {
+                header("Location:s_Addbuilding.php?msg=Give the building name");
+                die();
+        }
+        $query2="delete from Building where buildingName = '".$_POST['bname']."';";
+        execute($query2);
+        header("Location:s_Addbuilding.php?msg=Building Deleted");
+        die();
+}
+if(isset($_POST['add'])){                                       // add button
+	if($_POST['Bname']){
+        $building = escape($_POST['Bname']);
+//      dbconnect();
+        $q_t = "select buildingName from Building where buildingName='".$building."';";
+        $r_t = execute($q_t);
+        if(mysql_num_rows($r_t) !=0)
+        {
+                header("Location:s_Addbuilding.php?msg='Building already in the system'");
+                die();
+        }
+
+        $q = "INSERT INTO Building (buildingName) VALUES('".$building."');";
+	echo $q;
+        $r=execute($q);
+        if (mysql_affected_rows()){
+
+                header("Location:s_Addbuilding.php?msg=Building Added");
+                die();
+
+        }
+}
+}
+?>
