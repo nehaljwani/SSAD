9c502e1af8325ec80e519d96b479d15f5a343c25
Nehal J. Wani <nehaljw.kkd1@gmail.com>
2012-10-16 18:47:45 +0530

    acceptReject and requestDetails added

---
+++ acceptReject.php
@@ -0,0 +1,78 @@
+<?php
+include("essential.php");
+dbconnect();
+
+print_r($_POST);
+
+echo $_POST['forwardID'];
+$a="accept";
+$b="reject";
+$c="forward";
+$d= "Specify a reason for rejection (optional) )";
+$rID = $_POST['reqID'];
+
+$reqArray = getRequestByID($_POST['reqID']);
+print_r($reqArray);
+$clashArrays = clashMux(checkConflicts());
+
+foreach($clashArrays as $clashArray){
+	if(in_array($rID, $clashArray)){
+		break;
+	}
+}
+
+//clashArray now has all elements clashing with the current request, including the current request
+
+if($_POST['reqAction']==$b)
+{
+	if($_POST['reason']==$d)
+	{
+		$sq="update Requests set appStatus='Rejected', reqRejectReason='None' where reqNo=".$_POST['reqID'].";";
+	}
+	else
+	{
+		$sq="update Requests set appStatus='Rejected',reqRejectReason ='".$_POST['reason']."' where reqNo=".$_POST['reqID'].";";
+	}
+	$req = getRequestByID($rID);
+	reject($req['creator'], $req['creatorEmail'], $req['room'], $req['reqNo']);
+	reject($req['concernedPName'], $req['concernedPEmail'], $req['room'], $req['reqNo']);
+}
+else if($_POST['reqAction']==$a)
+{
+	$sq="update Requests set appstatus='Accepted' where reqNo=".$_POST['reqID'].";";
+	foreach($clashArray as $rejectThis){
+		if($rejectThis != $rID){
+			echo$rejectThis;
+			$otherQuery = "update Requests set appStatus = 'Rejected', reqRejectReason = 'A request conflicting with your request was accepted.' where reqNo = {$rejectThis}";
+			echo $otherQuery."\n";
+			//execute($otherQuery);
+
+			$req = getRequestByID($rejectThis);
+			reject($req['creator'], $req['creatorEmail'], $req['room'], $req['reqNo']);
+			reject($req['concernedPName'], $req['concernedPEmail'], $req['room'], $req['reqNo']);
+		}
+	}
+	$req = getRequestByID($rID);
+	accept($req['creator'], $req['creatorEmail'], $req['room'], $req['reqNo']);
+	accept($req['concernedPName'], $req['concernedPEmail'], $req['room'], $req['reqNo']);
+}
+else if($_POST['reqAction']==$c)
+{
+	$sq="update Requests set concernedAdmin = {$_POST['forwardID']} where reqNo = {$_POST['reqID']}";
+	$req = getRequestByID($rID);
+	$emails = getEmails($_POST['forwardID']);
+	print_r($emails);
+	foreach($emails as $email){
+		forward($req['concernedPName'], $email, $req['room'], $req['reqNo'], $req['concernedPEmail']);
+	}
+}
+echo $sq;
+//execute($sq);
+
+echo "<br />";
+
+
+print_r($clashArray);
+
+
+?>
---
+++ requestDetails.php
@@ -0,0 +1,10 @@
+<?php include_once("header.php"); ?>
+<?php include_once("essential.php"); ?>
+<div class="post">
+	<h2 class="title">Request Details</h2>
+	<div class="entry">
+
+	</div>
+
+</div>
+<?php include_once("footer.php"); ?>
