81719fd0c88ae9e494be336f63540aa197340976
nehaljwani <nehaljw.kkd1@gmail.com>
2012-11-14 00:11:25 +0530

    Js for predef.php

--- predef.php
+++ predef.php
@@ -2,6 +2,7 @@
 <center>
 <form name='predef' action="my.php" method='post'>
 <table align='center'>
+<script language="javascript" type="text/javascript" src="js/predef.js"></script>
 <h2 align='center'> Pre Allocate Rooms </h2><br/>
 <tr>
 <?php $rooms=array("H101","H102","H201","H202","H301","H302","B4-304","B4-301","B6-309","C1-302","SH1","SH2","CR1","CR2","H103","H104","H203","H204","H303","H304","N104");
@@ -52,7 +53,9 @@ while($i<$val)
 	$i++;
 }
 echo "</select></td></tr></table><br/><br/>";
-echo "<input type='submit' value='submit'> </form></center><br/>";
+echo "<input type='submit' value='submit'>
+<button type='reset' value='submit'>Reset</button>
+ </form></center><br/>";
 include('footer.php');
 ?>
 
