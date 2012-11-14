277101cec03100fe64fd88afae7f6e98e53e3125
shubham sangal <shubham@shubham-HP-dv6.(none)>
2012-11-14 19:01:46 +0530

    detail

--- details.php
+++ details.php
@@ -13,7 +13,7 @@ else{
 $sq="select * from Requests where reqNo =".$id.";";
 //echo $sq;
 $res=execute($sq);
-$col1=array("RequestID","hash","Request made by:","Requester Email","Requester Phone ","Concerned Person Phone ","Concerned Person Email"," Concerned Person Phone","Request Status","Group of Requester","Request Date","event Start Date","event End Date","event Start Time","event End Time ","Title of the Event","event Description","Days of Event","Id of Admin handling this request","room","request Type");
+$col1=array("RequestID","hash","Request made by:","Requester Email","Requester Phone ","Concerned Person Phone ","Concerned Person Email"," Concerned Person Phone","Request Status","Group of Requester","Request Date","Event Start Date","Event End Date","Event Start Time","Event End Time ","Title of the Event","Event Description","Days of Event","Id of Admin handling this request","Room","Request Type","Request for accept/reject","Request last modified");
 $num_row=mysql_num_fields($res);
 $col=mysql_fetch_row($res);
 
@@ -82,6 +82,13 @@ if(1){ ?>
 
 
 <?php while($i<$num_row){
+	if($col[0]==0)
+	{
+		echo "This room is reserved for Admin";
+		break;
+	}
+	else
+	{
 	if($i!=1)
 	{
 		if($i==17)
@@ -104,7 +111,7 @@ if(1){ ?>
 <?php
 	}
 	$i++;
-}
+}}
 ?>
 </table>
 </div>
